3a4b9cf3b00302560f7db68bfd5592f751d22376
private <private@Macmini2018.local>
2023-11-10 16:54:48 +0900

    sec02

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import GuestLayout from '@/Layouts/GuestLayout.vue';
+    // import Label from '@/Components/Label.vue';
+    // import Input from '@/Components/Input.vue';
+    import Label from '@/Components/InputLabel.vue';
+    import Input from '@/Components/TextInput.vue';
+
+    const emitTest = e => console.log(e)
+
+    // defineProps({
+    //     : 
+    // })
+</script>
+
+<template>
+    <GuestLayout>
+        <Label value="件名"></Label>
+        <Input
+            modelValue="初期値が入ります"
+            @update:modelValue="emitTest"
+            ></Input>
+    </GuestLayout>
+</template>
